d3bc3084c8f85b05268562ce8d4a731a9a1e73bd
Juraj <juraj.paska@siemens.healthineers.com>
2020-12-23 22:49:35 +0100

    section 3 done but needed to review

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,35 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+            int groupsCount = 5;
+            int gradeGroupSize = Students.Count / groupsCount;
+
+            List<double> grades = new List<double>();
+            foreach(var student in Students)
+            {
+                grades.Add(student.AverageGrade);
+            }
+
+            grades.Sort();
+
+            char g = 'A';
+            for (int i = 1; i < gradeGroupSize; i++, g++)
+            {
+                if (averageGrade >= grades[i * gradeGroupSize])
+                {
+                    return g;
+                }
+            }
+
+
+            return 'F';
+        }
     }
 }
